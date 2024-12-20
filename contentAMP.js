e11197acfb287a83c83df921914da0987b90c43c
César Diego Anovich <iddiegobis@gmail.com>
2024-12-20 17:02:06 -0300

    addedd

--- contentAMP.js
+++ contentAMP.js
@@ -6,11 +6,11 @@ if (adsContainer) {
     console.log("Div 'ads' encontrada. Adicionando o <amp-ad>...");
 
     var ampAd = document.createElement('amp-ad');
-    ampAd.setAttribute('width', '300');
-    ampAd.setAttribute('height', '250');
+    ampAd.setAttribute('width', '320');
+    ampAd.setAttribute('height', '50');
     ampAd.setAttribute('type', 'doubleclick');
     ampAd.setAttribute('data-slot', '/7542/parceiros/amomeupet');
-    ampAd.setAttribute('data-multi-size', '300x250');
+    ampAd.setAttribute('data-multi-size', '320x50');
     ampAd.setAttribute('data-lazy-fetch', 'true');
     ampAd.setAttribute('data-loading-strategy', '1');
     ampAd.setAttribute('data-enable-refresh', '45');
